39d154671c566b245d716e8f31d3615bdb90c764
2218079LEEEUNA <144421698+2218079LEEEUNA@users.noreply.github.com>
2023-11-03 16:15:08 +0900

    Create quiz.6.py

---
+++ quiz.6.py
@@ -0,0 +1,8 @@
+a = int(input("크리스마스 트리의 높이를 설정하세요: "))
+
+for i in range(a):
+    for j in range(a - i - 1):
+        print(" ", end="")
+    for k in range(2 * i + 1):
+        print("*", end="")
+    print()
